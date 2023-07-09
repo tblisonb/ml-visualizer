20300e12893cb251dbaa8c427e7bf2acdd2d8a92
tblisonb <lisonbeetanner@gmail.com>
2023-07-08 23:26:59 -0700

    Added error checking to parser.py and implemented stuff in analyzer.py
    There are some new functions for getting point totals in parser.py, and in analyzer.py, calculate_similar_submitters is implemented and returns a list of how closely the person voted to every other submitter. Also removed testMain.py, choosing instead to keep any tests local as they may contain personally-identifiable information.

--- analyzer.py
+++ analyzer.py
@@ -1,20 +1,27 @@
-from parser import MlParser
-
-
 class MlAnalyzer:
 
     def __init__(self, parser):
         self.parser = parser
 
-    def calculate_similar_submitters(self, submitter):
+    def calculate_similar_submitters(self, submitter_name):
+        #   If <submitter_name> did not vote in the round, there's nothing to do.
+        if (self.parser.get_absolute_total_points_for_submitter(submitter_name) == 0):
+            print("Submitter \"" + submitter_name + "\" did not vote in this round.")
+            return
         submitters = self.parser.get_submitters()
-        votes = [None] * len(submitters)
-        submitter_votes = self.parser.get_votes_for_submitter(submitter)
-        for i in range(len(submitters)):
-            votes[i] = self.parser.get_votes_for_submitter(submitters[i])
-        #   Data must be square.
-        assert len(votes) == len(votes[0])
-        net_diff = [None] * len(submitters)
-        for i in range(len(votes)):
-            for j in range(len(votes)):
-                net_diff[i] += votes[j][i]
+        submitter_votes = self.parser.get_song_votes_for_submitter(submitter_name)
+        submitter_vote_total = 0
+        net_diff = dict(zip(submitters, [0] * len(submitters)))
+        for person in submitters:
+            compare_votes = self.parser.get_song_votes_for_submitter(person)
+            assert(len(submitter_votes) == len(compare_votes))
+            #   <vote> should be a key,value pair where the key is the song name and the value is the number of points.
+            for song in submitter_votes:
+                net_diff[person] += abs(submitter_votes[song] - compare_votes[song])
+        #   Remove key,value pair for <submitter_name> as it's always zero since it's comparing the person to themself.
+        del net_diff[submitter_name]
+        sorted_list = sorted(net_diff.items(), key=lambda item: item[1])
+        adjusted_list = []
+        for pair in sorted_list:
+            adjusted_list.append((pair[0], 1 - (pair[1] / (2 * self.parser.get_absolute_total_points_for_submitter(submitter_name)))))
+        return adjusted_list
--- parser.py
+++ parser.py
@@ -64,7 +64,7 @@ class MlParser:
     def get_song_for_submitter(self, submitter_name):
         submitter_row = self.df.loc[self.df["Submitter"] == submitter_name]
         if (len(submitter_row["Song"]) < 1):
-            print("No song found for submitter \"" + submitter_name + "\"")
+            print("No song found for submitter \"" + submitter_name + "\".")
             return None
         return submitter_row["Song"].iloc[0]
 
@@ -76,7 +76,7 @@ class MlParser:
     def get_submitter_for_song(self, song_name):
         song_row = self.df.loc[self.df["Song"] == song_name]
         if (len(song_row["Submitter"]) < 1):
-            print("No submitter found for song \"" + song_name + "\"")
+            print("No submitter found for song \"" + song_name + "\".")
             return None
         return song_row["Submitter"].iloc[0]
 
@@ -87,6 +87,9 @@ class MlParser:
 
     def get_submitter_votes_for_song(self, song_name):
         song_row = self.df.loc[self.df["Song"] == song_name]
+        if (len(song_row["Song"]) < 1):
+            print("Song \"" + song_name + "\" not found.")
+            return None
         #   The first two columns are not point data so ignore them.
         return song_row.iloc[0, 2:].to_dict()
 
@@ -96,7 +99,36 @@ class MlParser:
     #   Gets a dictionary of vote counts for a given submitter; key = song, value = points.
 
     def get_song_votes_for_submitter(self, submitter_name):
+        if (submitter_name not in self.df):
+            print("Submitter \"" + submitter_name + "\" not found.")
+            return None
         submitter_col = self.df[submitter_name]
         #   Create a dictionary of two different lists; being the songs and the points awarded by <submitter_name> (these 
         # should always line up 1:1 assuming the data is square).
         return dict(zip(self.get_songs(), submitter_col))
+
+    ###########################################################################################################################
+    # function get_net_total_points_for_song(song_name)
+    #
+    #   Gets the total number of points awarded to <song_name> by all submitters.
+
+    def get_net_total_points_for_song(self, song_name):
+        song_row = self.df.loc[self.df["Song"] == song_name]
+        if (len(song_row["Song"]) < 1):
+            print("Song \"" + song_name + "\" not found.")
+            return None
+        #   The first two columns are not point data so ignore them.
+        return sum(song_row.iloc[0, 2:].tolist())
+
+    ###########################################################################################################################
+    # function get_absolute_total_points_for_submitter(submitter_name)
+    #
+    #   Gets the absolute total number of points awarded by <submitter_name> for all songs (meaning all votes positive and
+    #   negative are counted).
+
+    def get_absolute_total_points_for_submitter(self, submitter_name):
+        if (submitter_name not in self.df):
+            print("Submitter \"" + submitter_name + "\" not found.")
+            return None
+        submitter_col = self.df[submitter_name]
+        return sum(map(abs, submitter_col))
